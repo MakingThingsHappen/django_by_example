cb6bf378c59f585a1a0b70e7f8397be36b052ef0
yanzhao <yanzhao@mbyte.tech>
2016-07-10 17:13:42 +0800

    Reset password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^logout-then-login/$', views.logout_then_login, name='logout_then_login'),
+    url(r'^password-change/$', views.password_change, name='password_change'),
+    url(r'^password-change/done/$', views.password_change_done, name='password_change_done'),
+    url(r'^password-reset/$', views.password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', views.password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', views.password_reset_complete, name='password_reset_complete'),
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -104,3 +104,6 @@ LOGOUT_URL = reverse_lazy('logout')
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
