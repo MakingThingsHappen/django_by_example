a4ea231addf15681d72fa58d7b26bf3d78a2ad1c
yanzhao <yanzhao@mbyte.tech>
2016-07-18 17:48:51 +0800

    Optimizing QuerySets that involve related objects

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -42,7 +42,11 @@ def dashboard(request):
                                                        flat=True)
     if following_ids:
         # If user is following others, retrieve only their actions
-        actions = actions.filter(user_id__in=following_ids)
+        actions = actions.filter(
+                user_id__in=following_ids
+        ).select_related(
+               'user', 'user__profile'
+        ).prefetch_related('target')
     actions = actions[:10]
     return render(request,
                   'account/dashboard.html',
